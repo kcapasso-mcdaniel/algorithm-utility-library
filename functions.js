0ed88cfccf4c67b9e127127ca38338c1ebcee673
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-01 15:09:57 -0700

    add functions to library

--- functions.js
+++ functions.js
@@ -1,1 +1,48 @@
-// Your functions go here!
+// Convert Celsius to Fahrenheit
+// A1: using the mathematical conversion of celsius to fahrenheit
+// R: the fahrenheit temperature
+function convertMe(celsius) {
+   let fahrenheit = (celsius * 9) / 5 + 32;
+   console.log(fahrenheit);
+   return fahrenheit;
+}
+
+// Reverse the order of an string
+// A1: we must split the array, reverse the order of the elements, then join them back together
+// R:  reserver Array
+// function reverseMe(string) {
+//    return string.split("").reverse().join("");
+// }
+function reverseMe(string) {
+   var splitStr = string.split("");
+   console.log(splitStr);
+   var reverseStr = splitStr.reverse();
+   console.log(reverseStr);
+   var newArr = reverseStr.join("");
+   return newArr;
+}
+
+// Factorialize a Number
+// A1: multiply the input number by each consecutive number beginning with 2
+// R: the product of the newNum multiplied by each consecutive number
+function factorialize(num) {
+   let newNum = 1;
+   for (let multiplyBy = 2; multiplyBy <= num; multiplyBy++) {
+      newNum *= multiplyBy;
+   }
+   return newNum;
+}
+
+// Find the Longest Word in a String
+// A1: in order to compare something like a sentence we must first split the elements before we can compare them
+// A2: the sort function is used to compare the elements to each other
+// R: numerical value of the longest element of the string
+function longestElement(string) {
+   var strSplit = string.split(" ");
+   var wordLength = strSplit.sort(function (a, b) {
+      return b.length - a.length;
+   });
+   return wordLength[0].length;
+}
+
+// Return Largets Numbers in Arrays
