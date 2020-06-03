073750953c3bde3e7cbdc592c0d2b5baade8302f
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-03 12:08:47 -0700

    add fixes and proper notes

--- functions.js
+++ functions.js
@@ -2,35 +2,42 @@
 // A1: using the mathematical conversion of celsius to fahrenheit
 // R: the fahrenheit temperature
 function convertMe(celsius) {
+   // conversion method from celsius to fahrenehit
    let fahrenheit = (celsius * 9) / 5 + 32;
    console.log(fahrenheit);
+   // temperatrue in fahrenheit
    return fahrenheit;
 }
 
 // Reverse the order of an string
 // A1: we must split the array, reverse the order of the elements, then join them back together
-// R:  reserver Array
-// function reverseMe(string) {
-//    return string.split("").reverse().join("");
-// }
+// R:  reserve Array
 function reverseMe(string) {
+   // split the string in order to manipulate it
    var splitStr = string.split("");
    console.log(splitStr);
+   // reverse the order of the elements
    var reverseStr = splitStr.reverse();
    console.log(reverseStr);
+   // join the elements back together into a string
    var newArr = reverseStr.join("");
+   // new reverse ordered string
    return newArr;
 }
 
 // Factorialize a Number - multiply number by each consecutive number minus one
-// A1: newNum represents that starting point of the consecutive numbers that will be multiplied
-// A2:
-// R: the product of the newNum multiplied by each consecutive number
+// A1: newNum is where the consecutive numbers begin
+// A2: using a for loop we are iterate through the consecutive numbers and on each iteration the newNum is multiplied by 2
+// R: new number
 function factorialize(num) {
+   // newNum represents the first number we test
    let newNum = 1;
+   // 2 is used as the multiplier because it is the second number therefore the first number to be multiplied by
    for (let multiplyBy = 2; multiplyBy <= num; multiplyBy++) {
       newNum *= multiplyBy;
    }
+   console.log(newNum);
+   // return one number that is a result of it being multiplied by itself
    return newNum;
 }
 
@@ -39,13 +46,18 @@ function factorialize(num) {
 // A2: the sort function is used to compare the elements to each other
 // R: numerical value of the longest element of the string
 function longestElement(string) {
+   // split the string to compare
    var strSplit = string.split(" ");
+   // use sort to compare the length of the words
    var words = strSplit.sort(function (word1, word2) {
       var length1 = word1.length;
       var length2 = word2.length;
-      length2 - length1;
+      // return the words sorted from largest to smallest
+      return length2 - length1;
    });
-   var result = words.length[0].length;
+   // gives us a number representing the number of characters in the longest word
+   var result = words[0].length;
+   console.log(result);
    return result;
 }
 
@@ -58,9 +70,10 @@ function longestElement(string) {
 function largestNumber(arr) {
    var newArr = [];
    for (let i = 0; i < arr.length; i++) {
-      var largestNumber = arr[i][0];
+      var largestNumber = null;
       for (let j = 1; j < arr[i].length; j++) {
-         if (arr[i][j] > largestNumber) largestNumber = arr[i][j];
+         if (arr[i][j] > largestNumber || largestNumber == null)
+            largestNumber = arr[i][j];
       }
       newArr.push(largestNumber);
    }
@@ -70,7 +83,7 @@ function largestNumber(arr) {
 
 // Slice a String
 // A1: slice examines the targeted element of a given string to see if it exisits within the string
-// R: new string
+// R: returns a boolean value of true or false
 function confirmEnding(str, target) {
    var newStr = str.slice(str.length - target.length) === target;
    console.log(newStr);
@@ -85,19 +98,23 @@ function repeatStringNumTimes(str, num) {
    for (let i = 0; i < num; i++) {
       newStr += str;
    }
+   console.log(newStr);
    return newStr;
 }
 
-// // Truncate a String
-// //
-// //
-// function truncateString(str, num) {
-//    if (str.length > num) {
-//       return str.slice(0, num) + "...";
-//    } else {
-//       return str.slice(0, num);
-//    }
-// }
+// Truncate a String
+// A1: using an if statement compare the length of the string to the target index number
+// R1: if length is greater than number return slice portion of string with ("...") appended to it
+// R2: if length is less than number return string
+function truncateString(str, num) {
+   if (str.length > num) {
+      console.log(str);
+      return str.slice(0, num) + "...";
+   } else {
+      console.log(str);
+      return str.slice(0, num);
+   }
+}
 
 // // Finders Keepers
 // //
